42c478e8671c12951f3e8e1938ac9f9946fed91f
duydo131 <dotheduybk131@gmail.com>
2021-05-20 15:12:45 +0700

    add customhook

---
+++ facebookclone/src/hooks/useUser.jsx
@@ -0,0 +1,50 @@
+import callApiHttp from 'functions/callApiHttp'
+import React, { useContext, createContext, useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+const userContext = createContext()
+
+function ProvideUser({ children }) {
+    const user = useProvideUser()
+    return <userContext.Provider value={user}>{children}</userContext.Provider>
+}
+ProvideUser.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+
+function useUser() {
+    return useContext(userContext)
+}
+
+function useProvideUser() {
+    const [userInfo, setUserInfo] = useState(localStorage.getItem('user_info'))
+    useEffect(() => {
+        fetchUserInfo()
+    }, [])
+
+    const fetchUserInfo = async () => {
+        try {
+            const res = await callApiHttp({
+                url: `/user/${localStorage.getItem('user_id')}`,
+                method: 'GET',
+            })
+            const { data } = res
+            if (data.code === 1000) {
+                setUserInfo(data.data)
+                localStorage.setItem('user_info', JSON.stringify(data.data))
+            } else {
+                // console.log('error: ', data)
+            }
+        } catch (error) {
+            // console.log(`error: ${  error}`)
+        }
+    }
+
+    return {
+        userInfo,
+        setUserInfo,
+        fetchUserInfo,
+    }
+}
+
+export { ProvideUser, useUser, useProvideUser }
