b81a3b173cf41c7c0d1a9806e82e88665d0581c3
minhduc2kvp <minhduc2kvp@gmail.com>
2021-05-27 12:29:17 +0700

    fix: call video

--- facebookclone/src/commons/constants.jsx
+++ facebookclone/src/commons/constants.jsx
@@ -1,6 +1,9 @@
 export const REACT_APP_API_URL =
     process.env.REACT_APP_API_URL ||
-    'http://localhost:8000/'
+    'http://localhost:8000'
 export const SOCKET_URL =
     process.env.REACT_APP_API_URL ||
-    'http://localhost:8000/'
+    'http://localhost:8000'
+export const VIDEO_CALL_URL =
+process.env.VIDEO_CALL_URL ||
+'http://localhost:5000'
